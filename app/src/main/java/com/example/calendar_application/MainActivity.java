894e80fe00de3520398c0984e23fdf20dd9b4037
surag4 <47417243+surag4@users.noreply.github.com>
2020-06-11 18:54:15 +1000

    Prototype
    Calendar Application, Final Prototype
    Functionatilities - Add Event, View Event, Delete Event

--- app/src/main/java/com/example/calendar_application/MainActivity.java
+++ app/src/main/java/com/example/calendar_application/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CalendarView CalendarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView = (CalendarView)findViewById(R.id.calendar_view);
     }
 }
