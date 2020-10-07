b5977d0af653d4ddbb3de2c531d3fd51729832fa
surag04 <47417243+surag4@users.noreply.github.com>
2020-10-07 22:18:50 +1100

    final updates including in-app permissions(storage,camera). A new event can be created that can include name of event, description, location, date, time along with pictures (importing from gallery/ capturing from camera) Pictures taken from camera are saved in database, viewed. Pictures imported from gallery are saved in database, viewed Newly created events are saved in database. Saved events can be viewed. Saved Event can be updated (Edited).
    Surag has completed his task. He was not an imposter.

--- app/src/main/java/com/example/calendar_application/IndividualData.java
+++ app/src/main/java/com/example/calendar_application/IndividualData.java
@@ -62,8 +62,8 @@ String id = EventRecyclerAdapter.chaiyeko;
         ShareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(IndividualData.this, "Export to pdf starts from here boys", Toast.LENGTH_SHORT).show();
-                Toast.makeText(IndividualData.this, ""+eve.get(0), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(IndividualData.this, "Export to pdf starts from here boys", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(IndividualData.this, ""+eve.get(0), Toast.LENGTH_SHORT).show();
             }
         });
         editbutton.setOnClickListener(new View.OnClickListener() {
